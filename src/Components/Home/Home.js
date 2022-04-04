b63b15984901ea3aa83a45ea9da2492d84c78787
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-04-05 01:43:06 +0600

    UI updated

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,14 +4,20 @@ import image from '../../Images/watch.png';
 
 const Home = () => {
     return (
-        <div className='home'>
-            <div className='heading-main-part'>
-                <h1>Your next <span className='h-color-1'>Smart Watch</span></h1>
-                <h1><span className='h-color-2'>Your best Watch</span></h1>
-                <p className='heading-info'>Perfect smart gadget for your daily life. It has 20 sports mood with dedicated heart rate monitoring. 15 days battery life will help you more energetic in your busy life.</p>
+        <div>
+            <div className='home'>
+                <div className='heading-main-part'>
+                    <h1>Your next <span className='h-color-1'>Smart Watch</span></h1>
+                    <h1><span className='h-color-2'>Your best Watch</span></h1>
+                    <p className='heading-info'>Perfect smart gadget for your daily life. It has 20 sports mood with dedicated heart rate monitoring. 15 days battery life will help you energetic in your busy life.</p>
+                </div>
+                <div>
+                    <img src={image} alt="" />
+                </div>
             </div>
+
             <div>
-                <img src={image} alt="" />
+                <h1>Customer Reviews</h1>
             </div>
         </div>
 
