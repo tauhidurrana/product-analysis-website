25fb5f12b553cc66e04c3c4406c1a3033a5a06ba
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-04-05 19:26:42 +0600

    Hook code restored

--- src/Hooks/useReviews.js
+++ src/Hooks/useReviews.js
@@ -1,11 +1,13 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const useReviews = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+    const [reviews, setReviews] = useState([]);
+    useEffect(()=>{
+        fetch('reviews.json')
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+    },[])
+    return ([reviews, setReviews]);
 };
 
 export default useReviews;
