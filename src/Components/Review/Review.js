75a5b4d4f4f20c89686c5ec27117f5eb74d74f1a
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-04-05 15:26:51 +0600

    icon added

--- src/Components/Review/Review.js
+++ src/Components/Review/Review.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './Review.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 const Review = (props) => {
     const {name, picture, reviewData} = props.review;
@@ -8,7 +10,14 @@ const Review = (props) => {
             <img src={picture} alt="" />
             <h4>Name: {name}</h4>
             <p>Comment: {reviewData}</p>
-            <p>Ratings: </p>
+            <p>Ratings : 
+            <FontAwesomeIcon className='fa' icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon className='fa' icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon className='fa' icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon className='fa' icon={faStar}></FontAwesomeIcon>
+            <FontAwesomeIcon className='fa' icon={faStar}></FontAwesomeIcon>
+
+            </p>
         </div>
     );
 };
