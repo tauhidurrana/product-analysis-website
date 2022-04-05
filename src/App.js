34f2c8ffd32ba57601548e58e77f691248f94fce
tauhidurrana <tauhidurrahman.rana@gmail.com>
2022-04-05 17:07:04 +0600

    404 route error handling added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Reviews from './Components/Reviews/Reviews';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Blogs from './Components/Blogs/Blogs';
 import About from './Components/About/About';
+import NotFound from './Components/NotFound/NotFound';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
           <Route path='/dashboard' element={<Dashboard></Dashboard>}></Route>
           <Route path='/blogs' element={<Blogs></Blogs>}></Route>
           <Route path='/about' element={<About></About>}></Route>
+          <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
